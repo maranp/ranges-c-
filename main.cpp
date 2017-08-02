279c0c392d45539c96636c4221d5efd8dba74561
Maran Pakkirisamy <mpakkirisamy@caveonetworks.com>
2017-08-02 22:16:32 +0530

    update main.cpp. start next with transform.hpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include "iterator_range.hpp"
 #include <iostream>
+#include <vector>
+
+bool testTransform() {
+  std::vector<int> input = {1, 2, 3, 4, 5};
+  std::vector<int> expected = {2, 4, 6, 8,10};
+  std::vector<int> result;
+  return true;
+}
+
+template <typename FuncT>
+bool testResult(FuncT && function) {
+  return std::forward<FuncT>(function)();
+}
+
+void launchTests() {
+  std::cout << "testResult(testTransform): "
+      << std::boolalpha
+      << testResult(testTransform) << std::endl;
+
+}
 
 int main() {
-  std::cout << is_const_iterator<const int *>::value << std::endl;
+  std::cout << std::boolalpha << is_const_iterator<const int *>::value << std::endl;
+  launchTests();
   return 0;
 }
