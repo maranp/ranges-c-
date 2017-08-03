97b7c7d453307a0a8714cd3e2a5828780a6811c3
Maran Pakkirisamy <mpakkirisamy@caveonetworks.com>
2017-08-03 23:56:44 +0530

    add transform_adaptor and | operator that returns an iterator_range out of an input container and a transform adaptor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "iterator_range.hpp"
+#include "algorithm.hpp"
 #include <iostream>
 #include <vector>
 
@@ -6,12 +7,21 @@ bool testTransform() {
   std::vector<int> input = {1, 2, 3, 4, 5};
   std::vector<int> expected = {2, 4, 6, 8,10};
   std::vector<int> result;
+  auto doubledNumbers = input | ranges::view::transform(multiplyByTwo);
+  ranges::push_back(result, doubledNumbers);
   return true;
 }
 
+// if function has to be taken as universal reference
+// but what is the need?
+//template <typename FuncT>
+//bool testResult(FuncT && function) {
+//  return std::forward<FuncT>(function)();
+//}
+
 template <typename FuncT>
-bool testResult(FuncT && function) {
-  return std::forward<FuncT>(function)();
+bool testResult(FuncT function) {
+  return function();
 }
 
 void launchTests() {
