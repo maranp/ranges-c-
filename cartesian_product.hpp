861faaaf43dbc1d144b5b3bb851fb12527513d7a
Maran Pakkirisamy <mpakkirisamy@caveonetworks.com>
2017-09-14 02:22:38 +0530

    build cartesian product

--- cartesian_product.hpp
+++ cartesian_product.hpp
@@ -10,16 +10,26 @@
 
 #include "iterator_range.hpp"
 #include "variadic.hpp"
+#include "named_type.hpp"
 
 namespace ranges {
+// BeginIteratorRef is a reference type (std::reference_wrapper)
+// that holds the reference to iterator being distinguished as
+// begin iterator
+template <typename Range>
+using BeginIteratorRef = NamedTypeRef<Range, struct begin_context>;
+
+template <typename Range>
+using EndIteratorRef = NamedTypeRef<Range, struct end_context>;
 
 namespace view {
 
 template <typename... Ranges>
 auto cartesian_product(Ranges const &... ranges) {
-  return iterator_range<cartesian_product_iterator<typename Ranges::const_iterator...>>(
-      make_cartesian_product_iterator(ranges.begin()...),
-      make_cartesian_product_iterator(ranges.end()...)
+  return iterator_range<cartesian_product_iterator<
+      typename Ranges::const_iterator...>>(
+      make_cartesian_product_iterator(BeginIteratorRef<Ranges const>(ranges)...),
+      make_cartesian_product_iterator(EndIteratorRef<Ranges const>(ranges)...)
       );
 }
 
